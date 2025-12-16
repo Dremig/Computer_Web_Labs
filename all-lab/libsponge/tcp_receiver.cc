f25c8df11417cdf1530237036c0d761eb9a6c64c
Dremig <1466140007@qq.com>
2025-12-16 11:50:25 +0800

    tcp receiver

--- all-lab/libsponge/tcp_receiver.cc
+++ all-lab/libsponge/tcp_receiver.cc
@@ -1,16 +1,52 @@
 #include "tcp_receiver.hh"
 
-// Dummy implementation of a TCP receiver
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader &header = seg.header();
+
+    if (header.syn) {
+        if (_syn_received) {
+        }
+        _syn_received = true;
+        _isn = header.seqno;
+    }
+
+    if (!_syn_received) {
+        return;
+    }
+
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written() + 1;
+
+    uint64_t abs_seqno = unwrap(header.seqno, _isn, checkpoint);
+    
+    uint64_t stream_index = abs_seqno - 1 + (header.syn ? 1 : 0);
+
+    
+    if (header.syn && seg.payload().size() == 0) {
+        if (header.fin) {
+             _reassembler.push_substring("", 0, true);
+        }
+        return; 
+    }
+
+    _reassembler.push_substring(seg.payload().copy(), stream_index, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_syn_received) {
+        return nullopt;
+    }
 
-size_t TCPReceiver::window_size() const { return {}; }
+    uint64_t abs_ack = _reassembler.stream_out().bytes_written() + 1;
+
+    if (_reassembler.stream_out().input_ended()) {
+        abs_ack += 1;
+    }
+
+    return wrap(abs_ack, _isn);
+}
+
+size_t TCPReceiver::window_size() const {
+    return _capacity - _reassembler.stream_out().buffer_size();
+}
